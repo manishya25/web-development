b1e05a9827ac28b0d0c22e9ae570e860d9a563c2
manishya25 <37130919+manishya25@users.noreply.github.com>
2018-04-12 16:43:05 +0530

    Update contact_form.php

--- contact_form.php
+++ contact_form.php
@@ -1,77 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-
-		<meta charset="utf-8">
-		<meta name="keywords" content="Blockchain development,Blockchain development company,Blockchain service provider,Smart contracts,smart wallet technology,hyperledger developer,artificial intelligence,ai development,artificial intelligence deveopment,artificial intelligence deveopment company,ai robot,applications of artificial intelligence,machine intelligence,artificial intelligence programming,artificial intelligence research,computational intelligence,intelligence artificial,artificial brain,mobile development,mobile app development,application development,mobile application development,mobile application development company,android app development,android application development,ios developers,ios development,ios development company,web developer,website design,website developer,web design company,design a website,web development company,website developers,ecommerce website design,bot,bot development,bot development company,online bot,chatbot,bot app,robot bot,computer bot,message bot,bot developer,bot program,robot chat,create a bot for website,UX Design,ux,user experience design,ui design,user interface design,interface design,ui ux designer">
-		<meta name="description" content="We are a team of Metaoriginslab who has expertise in building Blockchain Technology,Smart Contracts,Dapps,Artificial intelligence,Mobile Development,Web Development,Bot,UX Design and many more.">
-		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-				<link rel="shortcut icon" href="images/favicon.ico" />
-
-	    <title>Metaorigin</title>
-		<meta name="keywords" content="">
-		<meta name="description" content="">
-		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		
-		<link rel="stylesheet" href="css/animate.min.css">
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/font-awesome.min.css">
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="css/templatemo-style.css">
-		<script src="js/jquery.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery.singlePageNav.min.js"></script>
-		<script src="js/typed.js"></script>
-		<script src="js/wow.min.js"></script>
-		<script src="js/custom.js"></script>
-	</head>
-	<body id="top">
-
-		<!-- start preloader -->
-		<div class="preloader">
-			<div class="sk-spinner sk-spinner-wave">
-     	 		<div class="sk-rect1"></div>
-       			<div class="sk-rect2"></div>
-       			<div class="sk-rect3"></div>
-      	 		<div class="sk-rect4"></div>
-      			<div class="sk-rect5"></div>
-     		</div>
-    	</div>
-    	<!-- end preloader -->
-
-        
-        <!-- start navigation -->
-        <?php include_once('includes/header.php'); ?> 
-                <!-- start navigation -->
- 
-
-	
+	<head> 
+	<title>contact form</title>
 	<link rel="stylesheet" href="style.css" type="text/css" media="all" />
-</head>
+	</head>
+	
+
 
 <body>
-    		<div class="container">
-    			<div class="row">
-    				<div class="col-md-12">
-    					<h2 class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s"><span>CONTACT US</span> </h2>
-    				</div>
-    	
-
-
-        <div class="col-md-6 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.3s">
-    					<div class="team-wrapper">
-    						<h2> Write to us</h2>
-    		<form class="form" name="myForm" action="mail.php" method="POST" required>
-            <p class="name">
-                       <label for="name">Name</label><br>
-                       <input type="text" name="name" id="name" style="width:400px" required />
-		</p>
-		
-		<p class="email">
-			<label for="email">Email</label><br>
-                        <input type="text" name="email" id="email" style="width:400px" required />
+    <div class="container">
+    <div class="row">
+    <div class="col-md-12">
+    <h2 class="wow bounceIn" data-wow-offset="50" data-wow-delay="0.3s"><span>CONTACT US</span> </h2>
+    </div>
+    	<div class="col-md-6 col-sm-6 col-xs-12 wow fadeIn" data-wow-offset="50" data-wow-delay="1.3s">
+    	<div class="team-wrapper">
+    	<h2> Write to us</h2>
+    	<form class="form" name="myForm" action="mail.php" method="POST" required>
+        <p class="name">
+        <label for="name">Name</label><br>
+        <input type="text" name="name" id="name" style="width:400px" required />
+        </p>
+	<p class="email">
+        <label for="email">Email</label><br>
+        <input type="text" name="email" id="email" style="width:400px" required />
 			
 		</p>
 
@@ -107,23 +59,5 @@
 
         <!-- end footer -->
         </body>
-
-    	
-    				
-	
-
-<!-- start contact link -->
-
-
-<script type="text/javascript">
-    
-    function call_url(url){
-     window.location.href="http://metaorigins.com/"+url;
-    }
-</script>
-<!-- contact link -->
-
-</body>
-
 </html>
 
